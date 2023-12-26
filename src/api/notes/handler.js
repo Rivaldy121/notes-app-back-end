a8d8786e6310c9f1a347ad1bc871d76649fe31a3
RivalDartmedia <123264773+RivalDartmedia@users.noreply.github.com>
2023-12-26 20:19:17 +0700

    postgres database implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -11,11 +11,11 @@ class NotesHandler {
     this.deleteNoteByIdHandler = this.deleteNoteByIdHandler.bind(this);
   }
 
-  postNoteHandler(request, h) {
+  async postNoteHandler(request, h) {
     this._validator.validateNotePayload(request.payload);
     const { title = 'untitled', body, tags } = request.payload;
 
-    const noteId = this._service.addNote({ title, body, tags });
+    const noteId = await this._service.addNote({ title, body, tags });
 
     const response = h.response({
       status: 'success',
@@ -28,8 +28,8 @@ class NotesHandler {
     return response;
   }
 
-  getNotesHandler() {
-    const notes = this._service.getNotes();
+  async getNotesHandler() {
+    const notes = await this._service.getNotes();
     return {
       status: 'success',
       data: {
@@ -38,9 +38,9 @@ class NotesHandler {
     };
   }
 
-  getNoteByIdHandler(request) {
+  async getNoteByIdHandler(request, h) {
     const { id } = request.params;
-    const note = this._service.getNoteById(id);
+    const note = await this._service.getNoteById(id);
     return {
       status: 'success',
       data: {
@@ -49,7 +49,7 @@ class NotesHandler {
     };
   }
 
-  putNoteByIdHandler(request) {
+  putNoteByIdHandler(request, h) {
     this._validator.validateNotePayload(request.payload);
     const { id } = request.params;
 
@@ -61,7 +61,7 @@ class NotesHandler {
     };
   }
 
-  deleteNoteByIdHandler(request) {
+  deleteNoteByIdHandler(request, h) {
     const { id } = request.params;
     this._service.deleteNoteById(id);
     return {
